996ccebb857dc7a23557fbd0c0f4a880fca5a202
yanliangliang <yanliangliang123>
2016-10-08 14:52:55 +0800

    重构

--- study/dubbo/apis/src/main/java/com/cmp/study/dubbo/api/Application.java
+++ study/dubbo/apis/src/main/java/com/cmp/study/dubbo/api/Application.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.ImportResource;
 @ComponentScan(basePackages = {"com.cmp.study.dubbo.api.accountcenter"})
 public class Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
