e790008ba9e319c472c4e6645d279f85f03cdc7f
yanliangliang <yanliangliang123>
2016-11-04 18:51:46 +0800

    M

--- mybatis-generator-ext/mybatis-generator-plugins/src/main/java/com/yanll/mybatis/generator/plugins/MapperPlugin.java
+++ mybatis-generator-ext/mybatis-generator-plugins/src/main/java/com/yanll/mybatis/generator/plugins/MapperPlugin.java
@@ -1,11 +1,35 @@
 package com.yanll.mybatis.generator.plugins;
 
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
 
 import java.util.List;
 
+/**
+ * Created by YANLL on 2016/11/4.
+ * <p>
+ * 基于Mybatis-generator的扩展插件，用于生成Mapper.java、Entity.java、Mapper.xml。
+ * 自动生成的代码和手写的代码应该分开文件存放。
+ * <p>
+ * 鉴于现有开发框架framework-core项目已经针对数据库增删改做了抽象（findById、modify等等），故Mapper.java无需额外生成，直接调用现有即可。
+ * <p>
+ * <p>
+ * 代码生成原则：
+ * 生成的不修改，修改的不生成。
+ * <p>
+ * 后期规划：
+ */
 public class MapperPlugin extends PluginAdapter {
+
+    private static final String WARN = "当前文件为MybatisGenerator自动生成，重新生成可能会被覆盖，请勿修改！";
 
     @Override
     public boolean validate(List<String> list) {
@@ -22,5 +46,54 @@ public class MapperPlugin extends PluginAdapter {
         introspectedTable.setDeleteByPrimaryKeyStatementId("deleteById");
         introspectedTable.setSelectByPrimaryKeyStatementId("findById");
         super.initialized(introspectedTable);
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addJavaDocLine("/*" + WARN + "*/");
+        //DO默认都增加DataEntity继承
+        topLevelClass.setSuperClass("DataEntity");
+        topLevelClass.addImportedType("com.commons.DataEntity");
+        return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+    }
+
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //DataEntity已经存在，此处忽略不生成。
+        if ("createDatetime".equals(field.getName())) return false;
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //DataEntity已经存在，此处忽略不生成。
+        if ("getCreateDatetime".equals(method.getName()) || "setCreateDatetime".equals(method.getName())) return false;
+        return super.modelGetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //DataEntity已经存在，此处忽略不生成。
+        if ("setCreateDatetime".equals(method.getName())) return false;
+        return super.modelGetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        XmlElement parentElement = document.getRootElement();
+        parentElement.addElement(0, new TextElement("<!--" + WARN + "-->"));
+        //增加deleteByIds接口的sql元素
+        String tableName = introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime();//数据库表名
+        XmlElement deleteByIdsElement = new XmlElement("delete");
+        deleteByIdsElement.addAttribute(new Attribute("id", "deleteByIds"));
+        StringBuffer sql_xml = new StringBuffer();
+        sql_xml.append("delete from ").append(tableName).append(" where id IN ");
+        sql_xml.append("<foreach collection=\"ids\" item=\"id\" separator=\",\" open=\"(\" close=\")\">");
+        sql_xml.append("#{id}");
+        sql_xml.append("</foreach>");
+        deleteByIdsElement.addElement(new TextElement(sql_xml.toString()));
+        parentElement.addElement(deleteByIdsElement);
+        return super.sqlMapDocumentGenerated(document, introspectedTable);
     }
 }
