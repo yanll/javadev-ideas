9d72065977d24006ae842e3c44f99172747f96bc
yanliangliang <yanliangliang123>
2016-11-08 17:47:04 +0800

    M

--- mybatis-generator-ext/mybatis-generator-plugins/src/main/java/com/yanll/mybatis/generator/plugins/MapperPlugin.java
+++ mybatis-generator-ext/mybatis-generator-plugins/src/main/java/com/yanll/mybatis/generator/plugins/MapperPlugin.java
@@ -1,18 +1,16 @@
 package com.yanll.mybatis.generator.plugins;
 
-import org.mybatis.generator.api.GeneratedJavaFile;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
-import org.mybatis.generator.api.dom.DefaultJavaFormatter;
-import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.Document;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.internal.DefaultShellCallback;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -109,60 +107,6 @@ public class MapperPlugin extends PluginAdapter {
         deleteByIdsElement.addElement(new TextElement(sql_xml.toString()));
         parentElement.addElement(deleteByIdsElement);
         return super.sqlMapDocumentGenerated(document, introspectedTable);
-    }
-
-    @Override
-    public List<GeneratedJavaFile> contextGenerateAdditionalJavaFiles(IntrospectedTable introspectedTable) {
-        List<GeneratedJavaFile> files = introspectedTable.getGeneratedJavaFiles();
-        if (files == null) return null;
-        for (GeneratedJavaFile file : files) {
-            CompilationUnit compilationUnit = file.getCompilationUnit();
-            String fileEncoding = file.getFileEncoding();
-            String targetPackage = file.getTargetPackage();
-            String targetProject = file.getTargetProject();
-            if (compilationUnit.isJavaInterface()) continue;
-            TopLevelClass original = (TopLevelClass) compilationUnit;
-            String baseRecordType = introspectedTable.getBaseRecordType() + "VO";
-            TopLevelClass newModel = new TopLevelClass(baseRecordType);
-            newModel.addJavaDocLine("/*");
-            newModel.addJavaDocLine("* " + "当前文件为MybatisGenerator自动生成的VO，请手动剪切到*-service项目。");
-            newModel.addJavaDocLine("*/");
-            newModel.addImportedTypes(compilationUnit.getImportedTypes());
-            newModel.addStaticImports(compilationUnit.getStaticImports());
-            newModel.setAbstract(false);
-            newModel.setStatic(false);
-            newModel.setFinal(false);
-            newModel.setVisibility(JavaVisibility.PUBLIC);
-            //VO默认都增加VoEntity继承
-            newModel.setSuperClass("VoEntity");
-            newModel.addImportedType("com.h2finance.framework.data.mysql.domain.VoEntity");
-            List<Field> fields = original.getFields();
-            if (fields != null) {
-                for (Field field : fields) {
-                    newModel.addField(field);
-                }
-            }
-            List<Method> methods = original.getMethods();
-            if (methods != null) {
-                for (Method method : methods) {
-                    newModel.addMethod(method);
-                }
-            }
-            GeneratedJavaFile f = new GeneratedJavaFile(newModel, targetProject, fileEncoding, new DefaultJavaFormatter());
-
-            File targetFile;
-            String source;
-            try {
-                DefaultShellCallback callback = new DefaultShellCallback(true);
-                File directory = callback.getDirectory(f.getTargetProject(), f.getTargetPackage());
-                targetFile = new File(directory, f.getFileName());
-                source = f.getFormattedContent();
-                PluginUtil.writeFile(targetFile, source, f.getFileEncoding());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
     }
 
 
